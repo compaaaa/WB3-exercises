476366d0b1ef91e02ebe302414c63369c8e0917d
compaaaa <azi.varela2003@gmail.com>
2022-10-03 10:56:13 -0400

    Added 3 functions

--- Functions/simple_functions.js
+++ Functions/simple_functions.js
@@ -1,17 +1,17 @@
 function favoriteThings(){
-    console.log("Haziel Varela");
+    console.log("Haziel");
     console.log("Inception");
     console.log("Zillakami");
 }
 
 
 function whyImHere(){
-    console.log("I joined this program because I heard it was a good alternative to college and could get me in a really good tech career path.");
+    console.log("I joined this program because I heard it was a good alternative to college and could get me paid a lot.");
 }
 
 
 function favoritePlace(){
-    console.log("My favorite place is Ponce City Market, I like the area and all the stores/restaurants inside.");
+    console.log("My favorite place is Florida, I like the cities that are near beaches like Sarasota.");
 }
 
 favoriteThings();
