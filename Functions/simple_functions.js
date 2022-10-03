4455fc66a969862ed0c1fe09c55c4b47ae9c7566
compaaaa <azi.varela2003@gmail.com>
2022-10-03 10:40:26 -0400

    Added 3 functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,19 @@
+function favoriteThings(){
+    console.log("Haziel Varela");
+    console.log("Inception");
+    console.log("Zillakami");
+}
+
+
+function whyImHere(){
+    console.log("I joined this program because I heard it was a good alternative to college and could get me in a really good tech career path.");
+}
+
+
+function favoritePlace(){
+    console.log("My favorite place is Ponce City Market, I like the area and all the stores/restaurants inside.");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
